d969aa95baacb8b51b7a8f9f1473251b71be3850
Nathaniel Kelso <nvkelso@gmail.com>
2015-07-15 18:39:44 -0700

    add interactivity: mouseover for name & click for key, values

--- main.js
+++ main.js
@@ -46,9 +46,104 @@ map = (function () {
 
     /***** Render loop *****/
 
+     // Feature selection
+    function initFeatureSelection () {
+        // Selection info shown on hover
+        var selection_info = document.createElement('div');
+        selection_info.setAttribute('class', 'label');
+        selection_info.style.display = 'block';
+
+        // Show selected feature on hover
+        scene.container.addEventListener('mousemove', function (event) {
+            var pixel = { x: event.clientX, y: event.clientY };
+
+            scene.getFeatureAt(pixel).then(function(selection) {
+                if (!selection) {
+                    return;
+                }
+                var feature = selection.feature;
+                if (feature != null) {
+                    // console.log("selection map: " + JSON.stringify(feature));
+
+                    var label = '';
+                    if (feature.properties.name != null) {
+                        label = feature.properties.name;
+                    }
+
+                    if (label != '') {
+                        selection_info.style.left = (pixel.x + 5) + 'px';
+                        selection_info.style.top = (pixel.y + 15) + 'px';
+                        selection_info.innerHTML = '<span class="labelInner">' + label + '</span>';
+                        scene.container.appendChild(selection_info);
+                    }
+                    else if (selection_info.parentNode != null) {
+                        selection_info.parentNode.removeChild(selection_info);
+                    }
+                }
+                else if (selection_info.parentNode != null) {
+                    selection_info.parentNode.removeChild(selection_info);
+                }
+            });
+
+            // Don't show labels while panning
+            if (scene.panning == true) {
+                if (selection_info.parentNode != null) {
+                    selection_info.parentNode.removeChild(selection_info);
+                }
+            }
+        });
+        
+        // Show selected feature on hover
+        scene.container.addEventListener('click', function (event) {
+            var pixel = { x: event.clientX, y: event.clientY };
+
+			scene.getFeatureAt(pixel).then(function(selection) {    
+				if (!selection) {
+					return;
+				}
+				var feature = selection.feature;
+				if (feature != null) {
+					// console.log("selection map: " + JSON.stringify(feature));
+
+					var label = '';
+					if (feature.properties != null) {
+						// console.log(feature.properties);
+						var obj = JSON.parse(JSON.stringify(feature.properties));
+						for (var x in feature.properties) {
+							var val = feature.properties[x]
+							label += "<span class='labelLine' key="+x+" value="+val+" onclick='setValuesFromSpan(this)'>"+x+" : "+val+"</span><br>"
+						}
+					}
+
+					if (label != '') {
+						selection_info.style.left = (pixel.x + 5) + 'px';
+						selection_info.style.top = (pixel.y + 15) + 'px';
+						selection_info.innerHTML = '<span class="labelInner">' + label + '</span>';
+						scene.container.appendChild(selection_info);
+					}
+					else if (selection_info.parentNode != null) {
+						selection_info.parentNode.removeChild(selection_info);
+					}
+				}
+				else if (selection_info.parentNode != null) {
+					selection_info.parentNode.removeChild(selection_info);
+				}
+			});
+			
+            // Don't show labels while panning
+            if (scene.panning == true) {
+                if (selection_info.parentNode != null) {
+                    selection_info.parentNode.removeChild(selection_info);
+                }
+            }
+        });
+        
+    }
+
     window.addEventListener('load', function () {
         // Scene initialized
         layer.on('init', function() {
+        	initFeatureSelection();
         });
         layer.addTo(map);
     });
